bea9803e8b1485faa37cf66ac9722bbeec80c077
Tina Stojic <tinastojic26@gmail.com>
2026-02-04 17:39:43 +0100

    omogucen pristup samo autentifikovanim korisnicima

--- upravljanjevremenom/routes/api.php
+++ upravljanjevremenom/routes/api.php
@@ -18,33 +18,46 @@ Route::middleware('auth:sanctum')-> group(function(){
 
 Route::get('/zadaci/{id}', [ZadatakController::class,'show']);
 Route::get('/zadaci', [ZadatakController::class,'index']);
-Route::post('/zadaci', [ZadatakController::class,'store']);
-Route::delete('/zadaci/{id}', [ZadatakController::class,'destroy']);
-Route::put('/zadaci/{id}', [ZadatakController::class,'update']);
+Route::middleware('auth:sanctum')-> group(function(){
+    Route::post('/zadaci', [ZadatakController::class,'store']);
+    Route::delete('/zadaci/{id}', [ZadatakController::class,'destroy']);
+    Route::put('/zadaci/{id}', [ZadatakController::class,'update']);
+});
 
 Route::get('/beleske/{id}', [BeleskeController::class,'show']);
 Route::get('/beleske', [BeleskeController::class,'index']);
-Route::post('/beleske', [BeleskeController::class,'store']);
-Route::delete('/beleske/{id}', [BeleskeController::class,'destroy']);
-Route::put('/beleske/{id}', [BeleskeController::class,'update']);
+Route::middleware('auth:sanctum')-> group(function(){
+    Route::post('/beleske', [BeleskeController::class,'store']);
+    Route::delete('/beleske/{id}', [BeleskeController::class,'destroy']);
+    Route::put('/beleske/{id}', [BeleskeController::class,'update']);
+});
 
 Route::get('/obavestenje/{id}', [ObavestenjeController::class,'show']);
 Route::get('/obavestenje', [ObavestenjeController::class,'index']);
-Route::post('/obavestenje', [ObavestenjeController::class,'store']);
-Route::delete('/obavestenje/{id}', [ObavestenjeController::class,'destroy']);
-Route::put('/obavestenje/{id}', [ObavestenjeController::class,'update']);
+Route::middleware('auth:sanctum')-> group(function(){
+    Route::post('/obavestenje', [ObavestenjeController::class,'store']);
+    Route::delete('/obavestenje/{id}', [ObavestenjeController::class,'destroy']);
+    Route::put('/obavestenje/{id}', [ObavestenjeController::class,'update']);
+
+});
 
 Route::get('/podsetnik/{id}', [PodsetnikController::class,'show']);
 Route::get('/podsetnik', [PodsetnikController::class,'index']);
-Route::post('/podsetnik', [PodsetnikController::class,'store']);
-Route::delete('/podsetnik/{id}', [PodsetnikController::class,'destroy']);
-Route::put('/podsetnik/{id}', [PodsetnikController::class,'update']);
+Route::middleware('auth:sanctum')-> group(function(){
+    Route::post('/podsetnik', [PodsetnikController::class,'store']);
+    Route::delete('/podsetnik/{id}', [PodsetnikController::class,'destroy']);
+    Route::put('/podsetnik/{id}', [PodsetnikController::class,'update']);
+});
 
 Route::get('/statistika/{id}', [StatistikaController::class,'show']);
 Route::get('/statistika', [StatistikaController::class,'index']);
-Route::post('/statistika', [StatistikaController::class,'store']);
-Route::delete('/statistika/{id}', [StatistikaController::class,'destroy']);
-Route::put('/statistika/{id}', [StatistikaController::class,'update']);
+Route::middleware('auth:sanctum')-> group(function(){
+    Route::post('/statistika', [StatistikaController::class,'store']);
+    Route::delete('/statistika/{id}', [StatistikaController::class,'destroy']);
+    Route::put('/statistika/{id}', [StatistikaController::class,'update']);
+});
+
+
 
 
 
